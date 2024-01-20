a6606acdb409bdda3faca3b501d560dac020400c
Teresa Moser <moserte@byui.edu>
2024-01-19 23:31:45 -0700

    save contact-detail file

--- cms/src/app/contacts/contact-detail/contact-detail.component.ts
+++ cms/src/app/contacts/contact-detail/contact-detail.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { Contact } from '../contact.model';
 
 @Component({
   selector: 'app-contact-detail',
   templateUrl: './contact-detail.component.html',
   styleUrl: './contact-detail.component.css'
 })
-export class ContactDetailComponent {
 
+export class ContactDetailComponent {
+  contact: Contact = new Contact(
+    "1", 
+    "R. Kent Jackson",
+    "jacksonk@byui.edu",
+    "208-496-3771", 
+    "../../assets/images/wdd430_document_wk02files/jacksonk.jpg"
+    );
+  
 }
--- cms/src/app/contacts/contact-list/contact-list.component.ts
+++ cms/src/app/contacts/contact-list/contact-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Contact } from '../contact.model'
 
 @Component({
   selector: 'app-contact-list',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrl: './contact-list.component.css'
 })
 export class ContactListComponent {
-
+  Contacts: Contact[] = [
+    new Contact("1", "R. Kent Jackson","jacksonk@byui.edu","208-496-3771", "../../assets/images/wdd430_document_wk02files/jacksonk.jpg", null),
+    new Contact("2", "Rex Barzee", "barzeer@byui.edu", "208-496-3768", "../../assets/images/wdd430_document_wk02files/barzeer.jpg", null)
+  ]
 }
