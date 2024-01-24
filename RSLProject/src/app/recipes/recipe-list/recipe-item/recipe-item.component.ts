dc6d990ab04cfaa0f1df1ea97aec4e40dd691b80
Teresa Moser <moserte@byui.edu>
2024-01-23 22:24:37 -0700

    Updates to RSLProject

--- RSLProject/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ RSLProject/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { Recipe } from '../../recipe.model';
 
 @Component({
   selector: 'app-recipe-item',
   templateUrl: './recipe-item.component.html',
   styleUrl: './recipe-item.component.css'
 })
-export class RecipeItemComponent {
+export class RecipeItemComponent implements OnInit {
+  @Input() recipe: Recipe;
+  @Output() recipeSelected = new EventEmitter<void>();
+
+  constructor() { }
+
+  ngOnInit(): void {
+    
+  }
+  onSelected() {
+    this.recipeSelected.emit();
+  }
 
 }
--- RSLProject/src/app/recipes/recipe-list/recipe-list.component.ts
+++ RSLProject/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,13 +7,20 @@ import { Recipe } from '../recipe.model';
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe','This is simply a test', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRn7hFmyFR447L9WnDB7PQ7Mh0rQBbthcGguQ&usqp=CAU' )
+    new Recipe('A Test Recipe','This is simply a test', 
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRn7hFmyFR447L9WnDB7PQ7Mh0rQBbthcGguQ&usqp=CAU' ),
+    new Recipe('A Test Recipe','This is simply a test', 
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRn7hFmyFR447L9WnDB7PQ7Mh0rQBbthcGguQ&usqp=CAU' )
   ];
   constructor() { }
 
   ngOnInit() {
+  }
 
+  onRecipeSelect(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
