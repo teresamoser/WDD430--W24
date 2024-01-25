a3fd637be5962b942d76964aaff196827a382c01
Teresa Moser <moserte@byui.edu>
2024-01-24 20:03:25 -0700

    save progress in tutorial

--- RSLProject/src/app/recipes/recipe-list/recipe-list.component.ts
+++ RSLProject/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -11,7 +12,7 @@ export class RecipeListComponent {
   recipes: Recipe[] = [
     new Recipe('A Test Recipe','This is simply a test', 
     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRn7hFmyFR447L9WnDB7PQ7Mh0rQBbthcGguQ&usqp=CAU' ),
-    new Recipe('A Test Recipe','This is simply a test', 
+    new Recipe('Another Test Recipe','This is simply a test', 
     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRn7hFmyFR447L9WnDB7PQ7Mh0rQBbthcGguQ&usqp=CAU' )
   ];
   constructor() { }
