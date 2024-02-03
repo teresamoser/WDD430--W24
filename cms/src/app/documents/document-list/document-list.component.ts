ec03972d570ad0d55ee1cd21239bfbf843e1b734
Teresa Moser <moserte@byui.edu>
2024-02-03 14:16:20 -0700

    Save Document Data

--- cms/src/app/documents/document-list/document-list.component.ts
+++ cms/src/app/documents/document-list/document-list.component.ts
@@ -11,10 +11,21 @@ export class DocumentListComponent {
   @Output() selectedDocumentEvent = new EventEmitter();
 
   Documents: Document[] = [
-    new Document('1', 'Document 1', 'This is Dummy Document 1.', 'https://www.byui.com/1' ),
-    new Document('2', 'Document 2', 'This is Dummy Document 2.', 'https://www.byui.com/2' ),
-    new Document('3', 'Document 3', 'This is Dummy Document 3.', 'https://www.byui.com/3' ),
-    new Document('4', 'Document 4', 'This is Dummy Document 4.', 'https://www.byui.com/4' ),
+    new Document('1', 'CIT 260 Programming with Classes', 
+        'This course will introduce the notion of classes and objects. It will present encapsulation at a conceptual level. It will also work with inheritance and polymorphism.', 
+        'https://www.byui.com/1' ),
+    new Document('2', 'CIT 366 Full Web Stack Developement', 
+        'This course will teach you how to design and build interactive web based applications using HTML, CSS, JavaScript, and a web development stack.', 
+        'https://www.byui.com/2' ),
+    new Document('3', 'CIT 425 Data Warehousing', 
+        ' learn how to build software to meet rigid specifications, how to optimize algorithmic performance, and how to more effectively utilize data structures in real-world applications.', 
+        'https://www.byui.com/3' ),
+    new Document('4', 'CIT 460 Web Services', 
+        'This course focuses on the backend development of dynamic, service-oriented web applications. Students will learn how to design and implement web services, how to interact with data storage, and how to use these tools to build functioning web applications', 
+        'https://www.byui.com/4' ),
+    new Document('4', 'CIT 495 Senior Practicum', 
+        'Write a senior project proposal outlining a professional-level experience.', 
+        'https://www.byui.com/4' ),
   ];
 
   onSelectedDocument(document: Document){
