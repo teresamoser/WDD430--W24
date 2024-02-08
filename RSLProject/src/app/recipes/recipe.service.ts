a081d5d4ee7d2a55427e2b50dd738ee43bed5a76
Teresa Moser <moserte@byui.edu>
2024-02-07 19:34:53 -0700

    Save shopping-list service to recipe.service

--- RSLProject/src/app/recipes/recipe.service.ts
+++ RSLProject/src/app/recipes/recipe.service.ts
@@ -1,17 +1,39 @@
-import { EventEmitter } from '@angular/core';
-import { Recipe } from './recipe.model';
+import { EventEmitter, Injectable } from '@angular/core';
 
+import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+
+@Injectable()
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('A Test Recipe','This is simply a test', 
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRn7hFmyFR447L9WnDB7PQ7Mh0rQBbthcGguQ&usqp=CAU' ),
-        new Recipe('Another Test Recipe','This is simply a test', 
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRn7hFmyFR447L9WnDB7PQ7Mh0rQBbthcGguQ&usqp=CAU' )
+        new Recipe(
+            'Empanadas (Beef Turnovers)',
+            'Pastry rounds and a mouthwatering filling of seasoned ground beef, tomatoes, onions, and garlic', 
+            'https://www.allrecipes.com/thmb/ClVUDKHEixA1Fu6kzjronzJcInc=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/1034561-empanadas-beef-turnovers-Janet-Henderson-4x3-1-a658a394cfc34f54b1ca408bc931f007.jpg',
+            [
+                new Ingredient('Beef', 1),
+                new Ingredient('Pastry', 10)
+            ] ),
+        new Recipe(
+            'Manicotti',
+            'This manicotti recipe makes a comforting dinner your family will love. The kids like to help stuff the noodles too! Delicious served with a crispy salad and garlic bread.', 
+            'https://www.allrecipes.com/thmb/fYBgHyOrTFXNc1nZMkHFDmk6QOU=/282x188/filters:no_upscale():max_bytes(150000):strip_icc():focal(639x0:641x2):format(webp)/Manicotti-19201058c06a4eb89ed6f4cc44e65bb7.jpg', 
+            [
+                new Ingredient('Manicotti', 12),
+                new Ingredient('Ricotta Cheese', 1)
+            ] )
       ]; 
+
+    constructor(private slService: ShoppingListService) {}
 
     getRecipes(){
         return this.recipes.slice();
+    }
+
+    addIngredientsToShoppingList(ingredients: Ingredient[]) {
+        this.slService.addIngredients(ingredients);
     }
 }
